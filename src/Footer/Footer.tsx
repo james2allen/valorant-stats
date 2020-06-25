a8c4554d3d8d249a391dcb7302689a322c668c05
James Allen <james2allen@gmail.com>
2020-06-25 15:57:35 -0400

    Added footer component

---
+++ src/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+/** Imports */
+import React from "react";
+import "./Footer.css";
+
+/** Main application component */
+function Footer({ appName }: { appName: string }) {
+  return (
+    <div>
+      {appName} isn't endorsed by Riot Games and doesn't reflect the views or
+      opinions of Riot Games or anyone officially involved in producing or
+      managing Riot Games properties. Riot Games, and all associated properties
+      are trademarks or registered trademarks of Riot Games, Inc.
+    </div>
+  );
+}
+
+export default Footer;
