77ed12f7e5ca428ec85fbfeb417277b55da0367f
James Allen <james2allen@gmail.com>
2020-06-25 17:38:29 -0400

    Restyled header and footer components

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,11 +1,11 @@
 /** Imports */
 import React from "react";
-import "./Footer.css";
+import "./Footer.scss";
 
 /** Footer component */
 function Footer({ appName }: { appName: string }) {
   return (
-    <div>
+    <div className='col-md-12 footer-text fixed-bottom'>
       {appName} isn't endorsed by Riot Games and doesn't reflect the views or
       opinions of Riot Games or anyone officially involved in producing or
       managing Riot Games properties. Riot Games, and all associated properties
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,10 +1,21 @@
 /** Imports */
 import React from "react";
-import "./Header.css";
+import "./Header.scss";
 
 /** Header application component */
 function Header({ appName }: { appName: string }) {
-  return <h1>{appName}</h1>;
+  function convertAppName(appName: string) {
+    let appHeader = appName
+      .split(" ")
+      .map((word) => word.toUpperCase())
+      .join("_");
+    return `_${appHeader}`;
+  }
+  return (
+    <div className='col-md-12 header-container'>
+      <h1 className='header-text'>{convertAppName(appName)}</h1>
+    </div>
+  );
 }
 
 export default Header;
