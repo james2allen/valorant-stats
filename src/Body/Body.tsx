c61a4bc532d44503df164b768e0d401161d32fe0
James Allen <james.allen@askuity.com>
2020-06-29 23:58:26 -0400

    Added background image for main page

--- src/Body/Body.tsx
+++ src/Body/Body.tsx
@@ -1,15 +1,20 @@
 /** Imports */
 import React from "react";
+import SearchBox from "../SearchBox/SearchBox";
+import valorantBg from "../common/images/valorant.jpg";
 import "./Body.scss";
-import NameSearch from "../NameSearch/NameSearch";
 
 /** Body component */
 function Body() {
   return (
     <div className='body-container'>
       <div className='container'>
-        <NameSearch></NameSearch>
+        <SearchBox></SearchBox>
       </div>
+      <img
+        src={valorantBg}
+        style={{ width: "100%", height: "auto", opacity: 0.7 }}
+      />
     </div>
   );
 }
