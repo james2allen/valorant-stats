98ee55cfea3c5da7efa52c3163a97b56ca4f767b
James Allen <james2allen@gmail.com>
2020-07-04 13:49:11 -0400

    Added test for searchbox validation

--- src/SearchBox/SearchBox.test.tsx
+++ src/SearchBox/SearchBox.test.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import SearchBox from "./SearchBox";
-import { create } from "react-test-renderer";
+import { shallow } from "enzyme";
+import isIdValid from "./Validation";
 
 describe("SearchBox component", () => {
   test("it renders the SearchBox component", () => {
-    const component = create(<SearchBox />);
-    const instance = component.getInstance();
-    expect(instance).toBeTruthy();
+    const component = shallow(<SearchBox />);
+    expect(component.find("SearchBox")).toBeTruthy();
+  });
+
+  test("it validates that Riot IDs and tags are entered in the correct format", () => {
+    // Truthy formats
+    expect(isIdValid("newName#NA1")).toBeTruthy();
+    expect(isIdValid("new Name#2345")).toBeTruthy();
+
+    // Falsy formats
+    expect(isIdValid("newName1")).toBeFalsy();
+    expect(isIdValid("#")).toBeFalsy();
+    expect(isIdValid("#sdf")).toBeFalsy();
+    expect(isIdValid("name ##1")).toBeFalsy();
   });
 });
---
+++ src/SearchBox/Validation.tsx
@@ -0,0 +1,18 @@
+/** Checks to see if the user has entered in a valid riot id format */
+const isIdValid = (id: string) => {
+  const splitId = id.split("#");
+  const idRegex = /([A-Za-z\s]+)/;
+  const tagRegex = /([A-Z0-9]{3,5})/;
+
+  if (splitId.length !== 2) {
+    return false;
+  }
+
+  if (!splitId[0].match(idRegex) || !splitId[1].match(tagRegex)) {
+    return false;
+  }
+
+  return true;
+};
+
+export default isIdValid;
