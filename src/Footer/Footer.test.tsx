bf1830ba123fd1e04d11a9717e6985b671402b36
James Allen <james.allen@askuity.com>
2020-07-11 14:38:39 -0400

    Added footer text validation

--- src/Footer/Footer.test.tsx
+++ src/Footer/Footer.test.tsx
@@ -1,8 +1,12 @@
 import React from "react";
-import { render } from "@testing-library/react";
 import Footer from "./Footer";
+import { shallow } from "enzyme";
 
 test("renders footer", () => {
-  const { getByText } = render(<Footer appName='test app' />);
-  expect(getByText).toBeInTheDocument();
+  const wrapper = shallow(<Footer appName='test app' />);
+  expect(wrapper.find(".footer-text").text()).toEqual(
+    "test app isn't endorsed by Riot Games and doesn't reflect the views or opinions of " +
+      "Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot " +
+      "Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc."
+  );
 });
