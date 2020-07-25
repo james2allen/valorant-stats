cf7043cfcadf8c23c1e0eb5d07a5abfae7f70db9
James Allen <james.allen@askuity.com>
2020-07-25 15:32:20 -0400

    Implemented region select dropdown

---
+++ src/SearchBox/RegionSelect.tsx
@@ -0,0 +1,35 @@
+/** Imports */
+import React, { useState } from "react";
+import "./SearchBox.scss";
+import { Dropdown } from "react-bootstrap";
+
+export const RegionSelect = () => {
+  const [locale, setLocale] = useState("NA1");
+
+  const DropdownItems = [
+    "BR1",
+    "EUN1",
+    "EUW1",
+    "JP1",
+    "KR",
+    "LA1",
+    "LA2",
+    "NA1",
+    "OC1",
+    "TR1",
+    "RU",
+  ].map((region, index) => (
+    <Dropdown.Item key={index} onClick={() => setLocale(region)}>
+      {region}
+    </Dropdown.Item>
+  ));
+
+  return (
+    <Dropdown>
+      <Dropdown.Toggle variant='secondary' id='dropdown-basic'>
+        {locale}
+      </Dropdown.Toggle>
+      <Dropdown.Menu>{DropdownItems}</Dropdown.Menu>
+    </Dropdown>
+  );
+};
